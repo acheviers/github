a35ce5d90ee9b8d24e142650a01cae6f1c6ae664
LENOVO <LENOVO@DESKTOP-C30NCKG>
2022-04-18 12:58:33 +0530

    First Commit

---
+++ Githubdemo1/src/main/java/com/pages/Loginpage.java
@@ -0,0 +1,9 @@
+package com.pages;
+
+public class Loginpage {
+	
+	public void loginPage() {
+		System.out.println("this is login page");
+	}
+
+}
---
+++ Githubdemo1/src/test/java/com/testcases/Logintest.java
@@ -0,0 +1,8 @@
+package com.testcases;
+
+public class Logintest {
+	public void loginTest() {
+		System.out.println("this is login test");
+	}
+
+}
